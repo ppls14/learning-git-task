c24f9dbc49aae1ee6daf875127b2eb106104085c
Remik Tkaczyk <r.t.kodilla@gmail.com>
2022-11-18 19:20:09 +0100

    Loop dictionary and display inscription

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,8 @@ shop_dict = {
     'piekarnia' : ['chleb', 'bułka', 'pączek'],
     'warzywniak' : ['marchew', 'seler', 'rukola']
 }
+name = "Lista zakupów"
+print(name)
+
+for shop, article in shop_dict.items():
+  print(f"Idę do {shop}, kupuję tu następujące rzeczy:\n\n{article}\n")
